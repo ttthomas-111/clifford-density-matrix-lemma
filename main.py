105520cf4cafb5bee9c0f3a81b30c0f51b79cbec
ttthomas-111 <18819669315@163.com>
2025-08-02 14:41:04 +0800

    update the result of single qubit system and two-qubit system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from clifford import main
 
 if __name__ == "__main__":
-    # choose the number of qubit (1 or 2)
-    qubit = 3
+    # choose the number of qubit (1 or 2 or 3)
+    qubit = 2
     main(qubit)
