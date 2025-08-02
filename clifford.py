e1de1e3c58c441c66f10116713d47c994d111389
ttthomas-111 <18819669315@163.com>
2025-08-02 14:38:07 +0800

    update to support 3-qubit system

--- clifford.py
+++ clifford.py
@@ -28,6 +28,32 @@ def build_BPT(n,a,b,c):
                 rho_head[i, j] = values[idx]
             matrices.append(rho_head)
         return matrices
+    
+def three_qubit_BPT(combo,f,batch_size=10000):
+    print("combine")
+    positions = [(i, j, k) for i in range(4) for j in range(4) for k in range(4) if (i,j,k) != (0,0,0)]
+    all_position_combinations = combinations(positions, 7)
+    batch = []
+    print("BPT")
+    for idx, positions_comb in enumerate(all_position_combinations):
+        rho_head = np.zeros((4,4,4))
+        rho_head[0,0,0] = 1.0
+        for idx, (i, j, k) in enumerate(positions_comb):
+            rho_head[i, j, k] = combo[idx]
+        batch.append(rho_head)
+        if len(batch) >= batch_size:
+            print("overflow...")
+            process_batch(batch,f)
+            batch = []
+    if batch:
+        process_batch(batch,f)
+
+def process_batch(batch,f):
+    for rho_head in batch:
+        rho = inverse_BPT(rho_head,8)
+        result = is_scaled_Hermitian_projection_matrix(rho)
+        statement(rho_head,result,rho,f)
+
 
 def inverse_BPT(rho_head,n):
     X = np.array([[0,1],[1,0]])
@@ -44,6 +70,13 @@ def inverse_BPT(rho_head,n):
         for i in range(0,4):
             for j in range(0,4):
                 rho += rho_head[i][j]*(np.kron(pauli_matrices[i]/2,pauli_matrices[j]/2))
+    elif n==8:
+        rho = np.zeros((n,n), dtype=np.complex128)
+        for i in range(0,4):
+            for j in range(0,4):
+                for k in range(0,4):
+                    term = rho_head[i][j][k]*(np.kron(pauli_matrices[i]/2,pauli_matrices[j]/2))
+                    rho += np.kron(term,pauli_matrices[k]/2)
     return rho
 
 def is_scaled_Hermitian_projection_matrix(rho, tol=1e-8):
@@ -83,14 +116,14 @@ def norm(a,b,c):
 def statement(rho_head,result,rho,f):
     if is_psd_matrix(rho):
         if result[0]==True:
-                f.write("case:\n" + str(rho_head) + ": " + str(result[0]) + "\n")
-                f.write("\n")
+            f.write("case:\n" + str(rho_head) + ": " + str(result[0]) + "\n")
+            f.write("\n")
         elif result[0]==False:
-                f.write("case:\n" + str(rho_head) + ": " + str(result[0])+"\n")
-                f.write("The counterexample is:\n")
-                f.write("rho_head:\n"+str(rho_head)+"\n")
-                f.write("rho:\n"+str(rho)+"\n")
-                f.write("\n")
+            f.write("case:\n" + str(rho_head) + ": " + str(result[0])+"\n")
+            f.write("The counterexample is:\n")
+            f.write("rho_head:\n"+str(rho_head)+"\n")
+            f.write("rho:\n"+str(rho)+"\n")
+            f.write("\n")
 
 def main(qubit):
     print("start")
@@ -127,3 +160,19 @@ def main(qubit):
                     rho = inverse_BPT(rho_head,n)
                     result = is_scaled_Hermitian_projection_matrix(rho)
                     statement(rho_head,result,rho,f)
+    if n==8:
+        with open("three-qubit.txt","w",encoding="utf-8") as f:
+            print(2)
+            all_permutations = list(itertools.product(values, repeat=7))
+
+            unique_combinations = []
+            seen = set()
+
+            for p in all_permutations:
+                sorted_p = tuple(sorted(p))
+                if sorted_p not in seen:
+                    seen.add(sorted_p)
+                    unique_combinations.append(sorted_p)
+            for combo in unique_combinations:
+                three_qubit_BPT(combo,f)
+ 
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from clifford import main
 
 if __name__ == "__main__":
     # choose the number of qubit (1 or 2)
-    qubit = 2
+    qubit = 3
     main(qubit)
